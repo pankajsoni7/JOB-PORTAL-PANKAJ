a37fd26ff8d1171d5f5728317770d75577ee0f62
Pankaj Soni <soni.pankaj4105@gmail.com>
2025-07-11 17:45:19 +0530

    Project Completed

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log('mongodb connected successfully');
+        console.log('mongodb is connected successfully');
     } catch (error) {
         console.log('Failed to connect', error);
     }
